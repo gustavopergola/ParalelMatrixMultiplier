dde5b63f93b16ac33df2c5b66cfdebf54c61853f
vipkry <lolpsxvipkry2@gmail.com>
2018-07-03 10:30:47 -0300

    alterações do lab

--- OpenMP/main.c
+++ OpenMP/main.c
@@ -5,10 +5,12 @@
 #include <string.h>
 #include <omp.h>
 
-#define M1_ROWS_LENGTH 1800
-#define M1_COLUMNS_LENGTH 1800
-#define M2_ROWS_LENGTH 1800
+#define M1_ROWS_LENGTH 3600
+#define M1_COLUMNS_LENGTH 3600
+#define M2_ROWS_LENGTH 3600
 #define M2_COLUMNS_LENGTH 3600
+
+#define THREADS 4
 
 bool matrizesNaoMultiplicaveis(){
     return M1_COLUMNS_LENGTH != M2_ROWS_LENGTH;
@@ -118,18 +120,18 @@ void calcula_matriz_resultante_sequencial(int (*matriz_a)[M1_COLUMNS_LENGTH], in
     free(resultMatrix);
 }
 
-void* matrix_multiplier_openmp(int rowsA, int colsA, int matrixA[rowsA][colsA], int rowsB, int colsB, int matrixB[rowsB][colsB]){
+void* matrix_multiplier_openmp_metodo1(int rowsA, int colsA, int matrixA[rowsA][colsA], int rowsB, int colsB, int matrixB[rowsB][colsB]){
 
     int i,j,k;
     int (*result)[colsB] = allocArray(M1_ROWS_LENGTH, M2_COLUMNS_LENGTH);
     #pragma omp parallel shared(matrixA,matrixB,result) private(i,j,k)
     {
-        #pragma omp for  schedule(static)
+        #pragma omp for schedule(static)
         for (i=0; i<rowsA; i++){
             for (j=0; j<colsB; j++){
                 result[i][j] = 0;
                 for (k=0; k<rowsB; k++){
-                    result[i][j] = result[i][j] + matrixA[i][k]*matrixB[k][j];
+                    result[i][j] = result[i][j] + matrixA[i][k] * matrixB[k][j];
                 }
             }
         }
@@ -139,7 +141,7 @@ void* matrix_multiplier_openmp(int rowsA, int colsA, int matrixA[rowsA][colsA], 
 
 void calcula_matriz_openmp(int (*matriz_a)[M1_COLUMNS_LENGTH], int (*matriz_b)[M2_COLUMNS_LENGTH]){
     double begin = omp_get_wtime();
-    int (*resultMatrix)[M2_COLUMNS_LENGTH] = matrix_multiplier_openmp(M1_ROWS_LENGTH, M1_COLUMNS_LENGTH, matriz_a, M2_ROWS_LENGTH, M2_COLUMNS_LENGTH, matriz_b);
+    int (*resultMatrix)[M2_COLUMNS_LENGTH] = matrix_multiplier_openmp_metodo1(M1_ROWS_LENGTH, M1_COLUMNS_LENGTH, matriz_a, M2_ROWS_LENGTH, M2_COLUMNS_LENGTH, matriz_b);
     double end = omp_get_wtime();
     double time_spent = end - begin;
     printf("Tempo decorrido utilizando OpenMP: %f\n", time_spent);
@@ -169,8 +171,8 @@ int main(int argc, char *argv[])
     //printf("Matriz B:\n");
     //mostraMatriz(M2_ROWS_LENGTH, M2_COLUMNS_LENGTH, matriz_b);
 
-    calcula_matriz_resultante_sequencial(matriz_a, matriz_b);
-    printf("calculo de matriz sequencial terminada!\n");
+    //calcula_matriz_resultante_sequencial(matriz_a, matriz_b);
+    //printf("calculo de matriz sequencial terminada!\n");
 
     calcula_matriz_openmp(matriz_a, matriz_b);
     printf("calculo de matriz utilizando OpenMP terminada!\n");
